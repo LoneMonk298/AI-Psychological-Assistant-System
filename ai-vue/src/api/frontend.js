8c5a98a405ce3a536678ed69c502bc07db086e62
engyujie <1391017400@qq.com>
2026-06-14 09:57:21 +0800

    添加知识库详情页面

--- ai-vue/src/api/frontend.js
+++ ai-vue/src/api/frontend.js
@@ -3,3 +3,7 @@ import service from "@/utils/request";
 export function getKnowledgeList(params) {
     return service.get('/knowledge/article/page', {params})
 }
+
+export function getKnowledgeDetail(articleId) {
+    return service.get(`/knowledge/article/${articleId}`)
+}
